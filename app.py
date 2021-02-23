a0bed6519e5e2ad232191ae5c344e26a82b4ef34
Rohith Chandran <59284674+rohith-cha@users.noreply.github.com>
2021-02-23 13:32:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # FLask API is a tool which helps to connect webs servers to your project
 
 from flask import Flask, render_template,url_for,request,jsonify
-import joblib
+from sklearn.externals import joblib
 import os
 import pickle
 import pandas as pd
